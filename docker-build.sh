edec9abd7367e72b60f70b371d8a5a683a178091
ChenTao <chentao_china@outlook.com>
2022-12-01 02:13:07 +0800

    format

--- docker-build.sh
+++ docker-build.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 ###
- # @Descripttion: 
- # @version: 
- # @Author: Tao Chen
- # @Date: 2022-12-01 02:07:32
- # @LastEditors: Tao Chen
- # @LastEditTime: 2022-12-01 02:09:22
-### 
+# @Descripttion:
+# @version:
+# @Author: Tao Chen
+# @Date: 2022-12-01 02:07:32
+# @LastEditors: Tao Chen
+# @LastEditTime: 2022-12-01 02:12:51
+###
 
 imageTag="$1"
 
 if [ -z "$imageTag" ]; then
-  imageTag=latest
+    imageTag=latest
 fi
 
 echo "Building sharelatex docker image tagged as [$imageTag]"
 # read -p "Press [Enter] to continue..." any_key;
 
 docker build --tag="sharelatex:$imageTag" . \
-  && echo "Built sharelatex image successfully tagged as sharelatex:$imageTag" \
-  && docker images "sharelatex:$imageTag"
+&& echo "Built sharelatex image successfully tagged as sharelatex:$imageTag" \
+&& docker images "sharelatex:$imageTag"
